19af668717bf65b5e866946e8924d7b1f8d68529
AdamRamberg <adam@mambojambostudios.com>
2019-07-31 00:43:49 +0200

    Breakout data from API

---
+++ data/songs.js
@@ -0,0 +1,100 @@
+const songs = [
+  {
+    name: 'Little Lion Man',
+    artist: 'Mumford & Sons',
+    numberOfPlays: 1000000,
+    length: 240,
+  },
+  {
+    name: 'The Cave',
+    artist: 'Mumford & Sons',
+    numberOfPlays: 900000,
+    length: 200,
+  },
+  {
+    name: 'Cobacabana',
+    artist: 'Mumford & Sons',
+    numberOfPlays: 700000,
+    length: 220,
+  },
+  {
+    name: 'I Will Wait',
+    artist: 'Mumford & Sons',
+    numberOfPlays: 800000,
+    length: 210,
+  },
+  {
+    name: 'Give Life Back to Music',
+    artist: 'Daft Punk',
+    numberOfPlays: 950000,
+    length: 400,
+  },
+  {
+    name: 'Motherboard',
+    artist: 'Daft Punk',
+    numberOfPlays: 750000,
+    length: 313,
+  },
+  {
+    name: 'Da Funk',
+    artist: 'Daft Punk',
+    numberOfPlays: 5000,
+    length: 200,
+  },
+  {
+    name: 'Digital Love',
+    artist: 'Daft Punk',
+    numberOfPlays: 5200,
+    length: 223,
+  },
+  {
+    name: 'Electric Feel',
+    artist: 'MGMT',
+    numberOfPlays: 950001,
+    length: 300,
+  },
+  {
+    name: 'Kids',
+    artist: 'MGMT',
+    numberOfPlays: 850001,
+    length: 301,
+  },
+  {
+    name: 'She Works Out Too Much',
+    artist: 'MGMT',
+    numberOfPlays: 750001,
+    length: 302,
+  },
+  {
+    name: 'When You Die',
+    artist: 'MGMT',
+    numberOfPlays: 650001,
+    length: 400,
+  },
+  {
+    name: 'Uprising',
+    artist: 'Muse',
+    numberOfPlays: 950002,
+    length: 600,
+  },
+  {
+    name: 'Starlight',
+    artist: 'Muse',
+    numberOfPlays: 850002,
+    length: 200,
+  },
+  {
+    name: 'Pressure',
+    artist: 'Muse',
+    numberOfPlays: 750002,
+    length: 100,
+  },
+  {
+    name: 'The Dark Side',
+    artist: 'Muse',
+    numberOfPlays: 650002,
+    length: 30,
+  },
+];
+
+module.exports = songs;
--- pages/api/songs.js
+++ pages/api/songs.js
@@ -1,90 +1,8 @@
 const stringHash = require('string-hash');
+const songs = require('../../data/songs');
 
-const SONGS = [
-  {
-    name: 'Little Lion Man',
-    artist: 'Mumford & Sons',
-    numberOfPlays: 1000000,
-  },
-  {
-    name: 'The Cave',
-    artist: 'Mumford & Sons',
-    numberOfPlays: 900000,
-  },
-  {
-    name: 'Cobacabana',
-    artist: 'Mumford & Sons',
-    numberOfPlays: 700000,
-  },
-  {
-    name: 'I Will Wait',
-    artist: 'Mumford & Sons',
-    numberOfPlays: 800000,
-  },
-  {
-    name: 'Give Life Back to Music',
-    artist: 'Daft Punk',
-    numberOfPlays: 950000,
-  },
-  {
-    name: 'Motherboard',
-    artist: 'Daft Punk',
-    numberOfPlays: 750000,
-  },
-  {
-    name: 'Da Funk',
-    artist: 'Daft Punk',
-    numberOfPlays: 5000,
-  },
-  {
-    name: 'Digital Love',
-    artist: 'Daft Punk',
-    numberOfPlays: 5200,
-  },
-  {
-    name: 'Electric Feel',
-    artist: 'MGMT',
-    numberOfPlays: 950001,
-  },
-  {
-    name: 'Kids',
-    artist: 'MGMT',
-    numberOfPlays: 850001,
-  },
-  {
-    name: 'She Works Out Too Much',
-    artist: 'MGMT',
-    numberOfPlays: 750001,
-  },
-  {
-    name: 'When You Die',
-    artist: 'MGMT',
-    numberOfPlays: 650001,
-  },
-  {
-    name: 'Uprising',
-    artist: 'Muse',
-    numberOfPlays: 950002,
-  },
-  {
-    name: 'Starlight',
-    artist: 'Muse',
-    numberOfPlays: 850002,
-  },
-  {
-    name: 'Pressure',
-    artist: 'Muse',
-    numberOfPlays: 750002,
-  },
-  {
-    name: 'The Dark Side',
-    artist: 'Muse',
-    numberOfPlays: 650002,
-  },
-];
-
-const transformSongs = songs =>
-  songs.map(song => ({
+const transformSongs = data =>
+  data.map(song => ({
     ...song,
     id: stringHash(`${song.name}___${song.artist}`).toString(),
   }));
@@ -94,7 +12,8 @@ export default (req, res) => {
   if (searchString) {
     res.setHeader('Content-Type', 'application/json');
     const popularSongs = transformSongs(
-      SONGS.sort((a, b) => b.numberOfPlays - a.numberOfPlays)
+      songs
+        .sort((a, b) => b.numberOfPlays - a.numberOfPlays)
         .filter(song =>
           searchString
             .split(' ')
@@ -110,10 +29,9 @@ export default (req, res) => {
   } else {
     res.setHeader('Content-Type', 'application/json');
     const popularSongs = transformSongs(
-      SONGS.sort((a, b) => b.numberOfPlays - a.numberOfPlays).slice(
-        0,
-        maxCount,
-      ),
+      songs
+        .sort((a, b) => b.numberOfPlays - a.numberOfPlays)
+        .slice(0, maxCount),
     );
     res.status(200).json(popularSongs);
   }
