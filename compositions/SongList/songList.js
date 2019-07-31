9c5f7dfae8f910d743c34554abc26a078d7d9c8d
AdamRamberg <adam@mambojambostudios.com>
2019-07-31 16:21:39 +0200

    Remove client side routing

--- compositions/SongList/songList.js
+++ compositions/SongList/songList.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { array } from 'prop-types';
-import Link from 'next/link';
 import styled from 'styled-components';
 import List, { ListItem } from '../../components/List';
 import { CenteredParagraph } from '../../components/Typography';
@@ -10,28 +9,29 @@ const StyledAnchor = styled.a`
   color: ${({ theme }) => theme.colors.onBackground};
 `;
 
+// NOTE: Use if to use client side routing
+// import Link from 'next/link';
+// eslint-disable-next-line react/prop-types
+/* const CurriedLink = ({ children, ...linkRest }) => (
+  <Link href="/playlist/[songId]" as={`/playlist/${id}`}>
+    <StyledAnchor href={`/playlist/${id}`} {...linkRest}>
+      {children}
+    </StyledAnchor>
+  </Link>
+); */
+
 const SongList = ({ songs, ...rest }) => (
   <List {...rest}>
     {songs.length ? (
-      songs.map(({ id, name, artist }) => {
-        // eslint-disable-next-line react/prop-types
-        const CurriedLink = ({ children, ...linkRest }) => (
-          <Link href="/playlist/[songId]" as={`/playlist/${id}`}>
-            <StyledAnchor href={`/playlist/${id}`} {...linkRest}>
-              {children}
-            </StyledAnchor>
-          </Link>
-        );
-        return (
-          <ListItem
-            key={id}
-            title={name}
-            subtitle={artist}
-            iconKey="arrow-right"
-            component={<CurriedLink />}
-          />
-        );
-      })
+      songs.map(({ id, name, artist }) => (
+        <ListItem
+          key={id}
+          title={name}
+          subtitle={artist}
+          iconKey="arrow-right"
+          component={<StyledAnchor href={`/playlist/${id}`} />}
+        />
+      ))
     ) : (
       <CenteredParagraph>
         Sorry, could not load most popular songs...
